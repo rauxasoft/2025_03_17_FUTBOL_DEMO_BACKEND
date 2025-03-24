53e5b2d03b15f5eb81f9c950e9ff1b55e96b466d
Jordi Alemany <jordia4@gmail.com>
2025-03-24 10:38:08 +0100

    arreglado bug arbitros... ahora se dan de alÃta

--- src/main/java/com/rauxasoft/liga_futbol/presentation/controllers/ArbitroController.java
+++ src/main/java/com/rauxasoft/liga_futbol/presentation/controllers/ArbitroController.java
@@ -6,11 +6,11 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +19,6 @@ import org.springframework.web.util.UriComponentsBuilder;
 import com.rauxasoft.liga_futbol.business.model.Arbitro;
 import com.rauxasoft.liga_futbol.business.services.ArbitroServices;
 import com.rauxasoft.liga_futbol.presentation.config.PresentationException;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @CrossOrigin
@@ -68,7 +66,7 @@ public class ArbitroController {
 			throw new PresentationException(e.getMessage(), HttpStatus.BAD_REQUEST);
 		}
 
-		return ResponseEntity.created(ucb.path("/productos/{id}").build(id)).build();
+		return ResponseEntity.created(ucb.path("/arbitros/{id}").build(id)).build();
 	}
 	
 	@PutMapping("/{id}")
